e16bd19d8db44da7c96733a4e10481e936be9d04
Ala' Hashesh <ala.hashesh@gmail.com>
2026-04-05 18:24:11 +0300

    chore: add tailwind

--- frontend/src/routes/users/index.tsx
+++ frontend/src/routes/users/index.tsx
@@ -5,5 +5,22 @@ export const Route = createFileRoute('/users/')({
 })
 
 const Users = () => {
-    return <div>Users</div>;
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <p className="text-gray-700 text-base">
+                    Users
+                </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                <span
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                <span
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                <span
+                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+            </div>
+        </div>
+    )
 }
